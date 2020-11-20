9106ee8aa4476bd507899f87774efa50111179cf
kmorrison <fspoon64+git@gmail.com>
2020-11-20 00:42:50 -0800

    use black on master

--- find_hero_test.py
+++ find_hero_test.py
@@ -4,16 +4,16 @@ herolist = opendota.get_hero_list()
 
 i = 1
 
-def find_hero(heroname): 
-  i=1
-  for hero in herolist: 
-    i+=1
-    if (herolist[i]['localized_name'] == heroname):
-      return(herolist[i])
 
-  
-  
-  print("Didn't find the hero")
-  return
+def find_hero(heroname):
+    i = 1
+    for hero in herolist:
+        i += 1
+        if herolist[i]["localized_name"] == heroname:
+            return herolist[i]
+
+    print("Didn't find the hero")
+    return
+
 
 print(find_hero("Juggernaut"))
--- opendota.py
+++ opendota.py
@@ -1,45 +1,48 @@
-import requests 
+import requests
 import json
 import secret
 
 
-#API_ROOT = 'https://api.opendota.com/api/matches/271145478?api_key=YOUR-API-KEY'
-API_ROOT = 'https://api.opendota.com/api'
+# API_ROOT = 'https://api.opendota.com/api/matches/271145478?api_key=YOUR-API-KEY'
+API_ROOT = "https://api.opendota.com/api"
 
-#get/find matches
+# get/find matches
 
 
 # def find_matches_by_hero():
 
+
 def find_hero(heroname):
-  for hero in load_hero_list():
-    if hero['localized_name'] == heroname:
-      return hero
-  print("Didn't find the hero")
-  return
+    for hero in load_hero_list():
+        if hero["localized_name"] == heroname:
+            return hero
+    print("Didn't find the hero")
+    return
 
-def get_hero_id(heroname): 
-  return find_hero(heroname)['id']
 
-def get_hero_list(): 
-  response = requests.get(
-    f'{API_ROOT}/heroes',
-    params = dict(api_key = secret.OPENDOTA_API_KEY)
-  )
-  return response.json()
+def get_hero_id(heroname):
+    return find_hero(heroname)["id"]
+
+
+def get_hero_list():
+    response = requests.get(
+        f"{API_ROOT}/heroes", params=dict(api_key=secret.OPENDOTA_API_KEY)
+    )
+    return response.json()
+
 
 def make_example_call():
     response = requests.get(
-        f'{API_ROOT}/matches/5705824607',
+        f"{API_ROOT}/matches/5705824607",
         params=dict(api_key=secret.OPENDOTA_API_KEY),
     )
     return response.json()
 
-def load_hero_list(): 
-  with open("hero_dict.json") as f: 
-    return json.loads(f.read())
+
+def load_hero_list():
+    with open("hero_dict.json") as f:
+        return json.loads(f.read())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
