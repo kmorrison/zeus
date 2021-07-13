c1cc02ceae131dd7a26f24731d032bbac7dc9530
kmorrison <fspoon64+git@gmail.com>
2021-07-12 18:45:53 -0700

    Add asserts for losemore

--- tests/test_matchlib.py
+++ tests/test_matchlib.py
@@ -72,3 +72,13 @@ def test_prune_winmore_stomp(stomp_match_data):
         advantage_threshold=500,
     )
     assert len(pruned_item_purchases[0]['purchases']) < len(original_purchase_data)
+
+    immortal_player_item_purchases = item_purchase_data[1]
+    original_purchase_data = immortal_player_item_purchases['purchases']
+    assert immortal_player_item_purchases['hero']['localized_name'] == 'Brewmaster'
+    pruned_item_purchases = matchlib.prune_winmore_purchases(
+        stomp_match_data, 
+        [immortal_player_item_purchases],
+        advantage_threshold=500,
+    )
+    assert len(pruned_item_purchases[0]['purchases']) < len(original_purchase_data)
