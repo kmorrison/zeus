e6b9f4b6506e6fce0c7992a37268becb175db60f
cadenforrest <cadenforrest@gmail.com>
2021-07-20 16:37:57 -0500

    changes per PR

--- item_rater.py
+++ item_rater.py
@@ -8,23 +8,7 @@ import matchlib
 import opendota
 
 
-def calculate_item_winrates_for_every_hero():
-    item_info = opendota.get_item_info()
-    heroes = calculate_item_winrates(item_info, start_time=None)
-
-    for hero_name, hero_info in heroes.items():
-        (
-            hero_info["games"],
-            hero_info["winrate"],
-            marginal_winrate,
-            marginal_cost_winrate,
-        ) = normalize_item_winrates_by_cost_and_hero_winrate(hero_info, item_info)
-        hero_info["marginal_winrate"] = marginal_winrate
-        hero_info["marginal_cost_winrate"] = marginal_cost_winrate
-        yield hero_name, hero_info
-
-
-def calculate_item_winrates(item_info, start_time, db_query):
+def calculate_item_winrates(item_info, db_query):
     heroes = {}
 
     for match in db_query:
@@ -72,7 +56,9 @@ def calculate_item_winrates(item_info, start_time, db_query):
     return heroes
 
 
-def normalize_item_winrates_by_cost_and_hero_winrate(hero_table, item_info, min_num_games = 30):
+def normalize_item_winrates_by_cost_and_hero_winrate(
+    hero_table, item_info, min_num_games=30
+):
     hero_winrate = hero_table.get("wins", 0) / hero_table.get("games", 1)
     marginal_winrate = []
     marginal_cost_winrate = []
@@ -111,23 +97,27 @@ if __name__ == "__main__":
     parser.add_argument("--start-time", type=int, default=0)
     parser.add_argument("--all-heroes", action="store_true")
     parser.add_argument("--min-num-games", type=int, default=30)
+    parser.add_argument("--num-hero-item-pairs", type=int, default=100)
     args = parser.parse_args()
 
     db = couchdb.get_matches_db()
     dbquery = couchdb.get_all_matches_with_hero_after_start_time(
-        db, args.start_time, args.hero,
+        db,
+        args.start_time,
+        args.hero,
     )
 
     item_info = opendota.get_item_table()
     item_winrates = calculate_item_winrates(
         item_info,
-        args.start_time,
         dbquery,
     )
+
     hero_winrates = [
         (hero_name, iw["wins"], iw["games"], (iw["wins"] / iw["games"]) * 100)
         for (hero_name, iw) in item_winrates.items()
     ]
+
     hero_winrates = sorted(hero_winrates, key=lambda x: x[3], reverse=True)
     print(
         tabulate.tabulate(hero_winrates, headers=["Name", "Wins", "Games", "Winrate"])
@@ -158,12 +148,13 @@ if __name__ == "__main__":
             games, winrate, a, b = normalize_item_winrates_by_cost_and_hero_winrate(
                 item_winrates[hero_name], item_info, args.min_num_games
             )
-            a_with_hero_name = [(*item, hero_name) for item in a ]
+            a_with_hero_name = [(*item, hero_name) for item in a]
             a_list.extend(a_with_hero_name)
 
         a_list.sort(key=lambda x: x[0], reverse=True)
         print(
             tabulate.tabulate(
-                a_list[:100], headers=["Marginal Winrate", "Total Games", "Item", "Hero"]
+                a_list[: args.num_hero_item_pairs],
+                headers=["Marginal Winrate", "Total Games", "Item", "Hero"],
             )
         )
